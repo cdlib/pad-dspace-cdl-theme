2fa9731e3259b9c7a83a00a7faf73e4d5aad3bdd
Hardy Pottinger <hardy.pottinger@gmail.com>
2023-08-04 12:18:42 -0500

    PUBD-975 remove duplicate line in eager-theme-module.ts

--- eager-theme.module.ts
+++ eager-theme.module.ts
@@ -8,8 +8,7 @@ import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-na
 import { RootModule } from '../../app/root.module';
 import { NavbarModule } from '../../app/navbar/navbar.module';
 import { SharedBrowseByModule } from '../../app/shared/browse-by/shared-browse-by.module';
-import { ResultsBackButtonModule } from '../../app/shared/results-back-button/results-back-button.module';import { SharedBrowseByModule } from '../../app/shared/browse-by/shared-browse-by.module';
-
+import { ResultsBackButtonModule } from '../../app/shared/results-back-button/results-back-button.module';
 
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
